5db8c548459e7cab5954f5ed71ca932283f5760c
rizkyauliaf <rizkyauliaf7@gmail.com>
2022-09-19 23:26:13 +0700

    third commit

--- resources/views/cetak_pdf.blade.php
+++ resources/views/cetak_pdf.blade.php
@@ -1,6 +1,8 @@
 <html>
 
 <head>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <style>
         table {
             width: 100%;
@@ -15,6 +17,8 @@
 </head>
 
 <body>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     <h2>Laporan Data Mahasiswa</h2>
     <table>
         <thead>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,22 +1,42 @@
 <html>
 
+<head>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+
 <body>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     @if(isset($success))
     <h4>{{$success}}</h4>
     @endif
 
     <form method="POST" action="{{url('mahasiswa')}}" enctype="multipart/form-data">
-        @csrf
-        Username <input type="text" name="username" required><br>
-        Password <input type="password" name="password" required><br>
-        Email <input type="email" name="email" required><br>
-        File <input type="file" name="berkas" required accept=".jpg,.png">
-        <button type="submit">Simpan</button>
+        {{csrf_field() }}
+        <div class="form-floating mb-3">
+            <input name="username" type="text" class="form-control" required>
+            <label for="username">Username</label>
+        </div>
+        <div class="form-floating">
+            <input name="password" type="password" class="form-control" required>
+            <label for="password">Password</label>
+        </div>
+        <div class="form-floating">
+            <input name="email" type="email" class="form-control" required>
+            <label for="email">Email</label>
+        </div>
+        <div class="mb-3">
+            <label for="formFile" class="form-label">Avatar</label>
+            <input name="berkas" class="form-control" type="file" required accept=".jpg,.png">
+        </div>
+        <div class="col-auto">
+            <button type="submit" class="btn btn-primary mb-3">Simpan</button>
+        </div>
 
         @if(isset($error))
         {{ $error }}
         @endif
-
     </form>
 </body>
 
